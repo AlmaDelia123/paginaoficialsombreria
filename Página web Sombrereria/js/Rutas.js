a139bffa8150a9c04efa21a1c51ffffa6654040a
AlmaDelia123 <103267117+AlmaDelia123@users.noreply.github.com>
2023-10-23 20:21:23 -0600

    Add files via upload

---
+++ Página web Sombrereria/js/Rutas.js
@@ -0,0 +1,19 @@
+document.getElementById("facebook").addEventListener("click", function() {
+    window.location.href = "http://www.facebook.com";
+});
+
+document.getElementById("twitter").addEventListener("click", function() {
+    window.location.href = "http://www.twitter.com";
+});
+
+document.getElementById("instagram").addEventListener("click", function() {
+    window.location.href = "http://www.instagram.com";
+});
+
+document.getElementById("google").addEventListener("click", function() {
+    window.location.href = "http://www.google.com";
+});
+
+document.getElementById("whatsapp").addEventListener("click", function() {
+    window.location.href = "http://www.whatsapp.com";
+});
